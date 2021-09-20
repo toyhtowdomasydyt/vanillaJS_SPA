1206cf01b71c560e98508f4d971eb662bf3ef1f9
Joel D <imaginethere.snoie@gmail.com>
2021-09-20 12:27:09 +0300

    Make no refresh page navigation

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
 const router = async () => {
     const routes = [
         {path: "/", view: () => console.log("Dashboard")},
@@ -26,6 +31,15 @@ const router = async () => {
     console.log(match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
    router();
 });
