9e77c7dae97269af9cc18c4eea6fa2ec4a849991
Joel D <imaginethere.snoie@gmail.com>
2021-09-20 12:35:36 +0300

    Make abstract class for all views

---
+++ frontend/static/js/views/AbstractView.js
@@ -0,0 +1,13 @@
+export default class AbstractView {
+    constructor() {
+
+    }
+
+    setTitle(title) {
+        document.title = title;
+    }
+
+    async getHTML() {
+        return "";
+    }
+}
